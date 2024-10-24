7b3bdc2373a5aa34673ce49ef6c8e4c7070a320f
imsidkg <siddharth8271skg2@gmail.com>
2024-10-25 02:47:46 +0530

    routes added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
+'use client'
+import { useCurrent } from "@/components/features/auth/api/useCurrent";
+import { useLogout } from "@/components/features/auth/api/useLogout";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router   = useRouter();
+  const {data , isLoading} = useCurrent();
+  const {mutate} = useLogout();
+
+  useEffect(() => {
+  if(!data && !isLoading) {
+    router.push('/signup')
+  }
+  }, [data])
   return (
     <div>
-      <Button variant="primary">Button</Button>
-      <Button variant="secondary">button</Button>
-      <Button variant="tertiary">button</Button>
-      <Button variant="outline">522222-</Button>
-      <Button variant="muted">++++++++++++</Button>
-      <Button variant="ghost">8888888888888888888888888888--</Button>
-      <Button variant="destructive">uguggu</Button>
+      only visible to you
+
+      <Button onClick={() => mutate()}>lougout</Button>
     </div>
   );
 }
--- src/components/features/auth/server/route.ts
+++ src/components/features/auth/server/route.ts
@@ -5,8 +5,13 @@ import { deleteCookie, setCookie } from "hono/cookie";
 import { loginSchmea, registrationSchema } from "../Schemas";
 import { createAdminClient } from "@/lib/appwrite";
 import { AUTH_COOKIE } from "@/lib/constants";
+import { sessionMiddleware } from "@/lib/sessionMiddleware";
 
 const app = new Hono()
+  .get("/current", sessionMiddleware, async (c) => {
+    const user = c.get("user");
+    return c.json({ data: user });
+  })
   .post("/login", zValidator("json", loginSchmea), async (c) => {
     const { email, password } = await c.req.valid("json");
 
@@ -39,6 +44,12 @@ const app = new Hono()
       maxAge: 60 * 60 * 24 * 30,
     });
 
+    return c.json({ success: true });
+  })
+  .post("/logout", sessionMiddleware, async (c) => {
+    deleteCookie(c, AUTH_COOKIE);
+    const account = c.get("account");
+    account.deleteSession("current");
     return c.json({ success: true });
   });
 
