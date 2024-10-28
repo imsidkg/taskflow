3828cd560a991dda5566f2c54f2eefeb3afadd8c
imsidkg <siddharth8271skg2@gmail.com>
2024-10-28 11:55:15 +0530

    workspace switcher added

--- src/components/WorkspaceSwitcher.tsx
+++ src/components/WorkspaceSwitcher.tsx
@@ -1,14 +1,60 @@
 'use client'
 import React from 'react'
 import { useGetWorkspaces } from './features/workspaces/api/useGetWorkspaces'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { RiAddCircleFill } from "react-icons/ri";
+import { useRouter } from 'next/navigation';
+import WorkspaceAvatar from './features/workspaces/components/WorkspaceAvatar';
 
 type Props = {}
 
 const WorkspaceSwitcher = (props: Props) => {
-    const {data , isLoading} = useGetWorkspaces(); 
+  // const workspaceId = useWorkspaceId();
+  const router = useRouter();
+  const { data: workspaces } = useGetWorkspaces();
+  // const { open } = useCreateWorkspaceModal();
+
+  const onSelect = (id: string) => {
+    router.push(`/workspaces/${id}`);
+  };
+
   return (
-    <div>{JSON.stringify(data)}</div>
-  )
+    <div className="flex flex-col gap-y-2">
+      <div className="flex items-center justify-between">
+        <p className="text-xs font-semibold uppercase text-neutral-500">
+          Workspaces
+        </p>
+        <RiAddCircleFill
+          // onClick={open}
+          className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"
+        />
+      </div>
+      <Select onValueChange={onSelect} >
+        <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
+          <SelectValue placeholder="No workspace selected" />
+        </SelectTrigger>
+        <SelectContent>
+          {workspaces?.documents.map((workspace) => (
+            <SelectItem key={workspace.$id} value={workspace.$id}>
+              <div className="flex justify-start items-center gap-3 font-medium">
+                <WorkspaceAvatar
+                  name={workspace.name}
+                  image={workspace.imageUrl}
+                />
+                <span className="truncate">{workspace.name}</span>
+              </div>
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
 }
 
 export default WorkspaceSwitcher
