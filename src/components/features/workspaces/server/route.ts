faa7db0fa8160047b99a314ef6a89ace4868575a
imsidkg <siddharth8271skg2@gmail.com>
2024-10-28 12:13:19 +0530

    member post request added in workspaces route

--- src/components/features/workspaces/server/route.ts
+++ src/components/features/workspaces/server/route.ts
@@ -2,56 +2,72 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/sessionMiddleware";
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
+import {
+  DATABASE_ID,
+  IMAGES_BUCKET_ID,
+  MEMBERS_ID,
+  WORKSPACES_ID,
+} from "@/config";
 import { ID } from "node-appwrite";
+import { MemberRole } from "../../members/types";
 
-const app = new Hono().get(
-  "/",
-  sessionMiddleware, 
-  async (c) => {
-    const databases = c.get('databases');
+const app = new Hono()
+  .get("/", sessionMiddleware, async (c) => {
+    const databases = c.get("databases");
     const workspaces = await databases.listDocuments(
       DATABASE_ID,
       WORKSPACES_ID
-
-    )
-   return c.json({data : workspaces})
-  }
-).post(
-  "/",
-  zValidator("form", createWorkspaceSchema),
-  sessionMiddleware,
-  async (c) => {
-    const databases = c.get("databases");
-    const storage = c.get("storage");
-    const user = c.get("user");
-    const { name, image } = c.req.valid("form");
-    let uploadedImageUrl: string | undefined;
-    if (image instanceof File) {
-      const file = await storage.createFile(
-        IMAGES_BUCKET_ID,
-        ID.unique(),
-        image
-      );
-      const arrayBuffer = await storage.getFilePreview(
-        IMAGES_BUCKET_ID,
-        file.$id,
     );
-    uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
-    }
-    const workspace = await databases.createDocument(
-      DATABASE_ID,
-      WORKSPACES_ID,
-      ID.unique(),
-      {
-        name,
-        userId: user.$id,
+    return c.json({ data: workspaces });
+  })
+  .post(
+    "/",
+    zValidator("form", createWorkspaceSchema),
+    sessionMiddleware,
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
+      const { name, image } = c.req.valid("form");
+      let uploadedImageUrl: string | undefined;
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image
+        );
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id
+        );
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+          arrayBuffer
+        ).toString("base64")}`;
       }
-    );
-    return c.json({
-      data: workspace,
-    });
-  }
-);
+      const workspace = await databases.createDocument(
+        DATABASE_ID,
+        WORKSPACES_ID,
+        ID.unique(),
+        {
+          name,
+          userId: user.$id,
+        }
+      );
+
+      const members = await databases.createDocument(
+        DATABASE_ID,
+        MEMBERS_ID,
+        ID.unique(),
+        {
+          userId: user.$id,
+          workspaceId: workspace.id,
+          role: MemberRole.ADMIN,
+        }
+      );
+      return c.json({
+        data: workspace,
+      });
+    }
+  );
 
 export default app;
