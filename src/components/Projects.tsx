8454534ac520e3cc1627ebc9cbe047f6005f413b
imsidkg <siddharth8271skg2@gmail.com>
2024-11-21 00:26:57 +0530

    projects schema added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,14 +4,17 @@ import { RiAddCircleFill } from 'react-icons/ri'
 import { useGetWorkspaces } from './features/workspaces/api/useGetWorkspaces'
 import { useWorkspaceId } from './features/workspaces/hooks/useWorkspaceId'
 import { useGetProjects } from './features/projects/api/useGetProjects'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 type Props = {}
 
 const Projects = (props: Props) => {
-
+  const pathname = usePathname();
   const workspaceId  = useWorkspaceId();
-  const query = useGetProjects({workspaceId});
-  const data = query.data;
+  const {data} = useGetProjects({workspaceId});
+
   return (
     <div className="flex flex-col gap-y-2">
     <div className="flex items-center justify-between">
@@ -23,6 +26,24 @@ const Projects = (props: Props) => {
         className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"
       />
     </div>
+
+    {data?.documents.map((project) => {
+      const href = `/workspaces/${workspaceId}/projects/${project.$id}`;
+      const isActive = pathname == href
+      return (
+        <Link href={href} key={project.$id}>
+        <div
+          className={cn(
+            "flex items-center gap-2.5 p-2.5 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500",
+            isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
+          )}
+        >
+          {/* <ProjectAvatar image={project.imageUrl} name={project.name} /> */}
+          <span className="truncate">{project.name}</span>
+        </div>
+      </Link>
+      )
+    })}
     </div>
   )
 }
