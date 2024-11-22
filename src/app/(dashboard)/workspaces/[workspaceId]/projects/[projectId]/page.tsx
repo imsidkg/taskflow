124c99372a2d2d0ea6fc1315180967e049b3ec8b
imsidkg <siddharth8271skg2@gmail.com>
2024-11-22 13:10:39 +0530

    project ui

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,10 +1,54 @@
+import { getCurrent } from '@/components/features/auth/actions'
+import { getProject } from '@/components/features/projects/actions';
+import { ProjectAvatar } from '@/components/features/projects/components/ProjectAvatar';
+import { Button } from '@/components/ui/button';
+import { PencilIcon } from 'lucide-react';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-type Props = {}
+type Props = {
+  params : {
+    projectId : string
+  }
+}
 
-const page = (props: Props) => {
+const page = async({params}: Props) => {
+  const user = await getCurrent();
+  if(!user) redirect ("/signin")
+
+    const initialValues = await getProject({
+      projectId : params.projectId
+    })
+
+    if(!initialValues) {
+      return new Error ("Project not found")
+    }
   return (
-    <div>redirected page</div>
+    <div className="flex flex-col gap-y-4">
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-x-2">
+        <ProjectAvatar
+          name={initialValues.name}
+          image={initialValues.imageUrl}
+          className="size-8"
+        />
+        <p className="text-lg font-semibold">{initialValues.name}</p>
+      </div>
+      <div className="">
+          <Button variant="secondary" size="sm" asChild>
+            <Link
+              href={`/workspaces/${initialValues.workspaceId}/projects/${initialValues.$id}/settings`}
+            >
+              <PencilIcon className="size-4 mr-2" />
+              Edit Project
+            </Link>
+          </Button>
+        </div>
+     
+    </div>
+ 
+  </div>
   )
 }
 
