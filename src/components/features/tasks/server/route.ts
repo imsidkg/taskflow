cb9d940277796eda9eff61718216af041581f57f
imsidkg <siddharth8271skg2@gmail.com>
2024-11-25 16:59:07 +0530

    task get api

--- src/components/features/tasks/server/route.ts
+++ src/components/features/tasks/server/route.ts
@@ -3,10 +3,137 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createTaskSchema } from "../schemas";
 import { getMember } from "../../members/utils";
-import { DATABASE_ID, TASKS_ID } from "@/config";
+import { DATABASE_ID, MEMBERS_ID, PROJECTS_ID, TASKS_ID } from "@/config";
 import { ID, Query } from "node-appwrite";
+import { createAdminClient } from "@/lib/appwrite";
+import { z } from "zod";
+import { Task, TaskStatus } from "../types";
+import { Project } from "../../projects/types";
 
-const app = new Hono().post(
+const app = new Hono().get(
+    "/",
+    sessionMiddleware,
+    zValidator(
+        "query",
+        z.object({
+            workspaceId: z.string(),
+            projectId: z.string().nullish(),
+            assigneeId: z.string().nullish(),
+            status: z.nativeEnum(TaskStatus).nullish(),
+            search: z.string().nullish(),
+            dueDate: z.string().nullish(),
+        })
+    ),
+    async (c) => {
+        const user = c.get("user");
+        const databases = c.get("databases");
+        const { users } = await createAdminClient();
+        const {
+            workspaceId,
+            projectId,
+            assigneeId,
+            status,
+            search,
+            dueDate
+        } = c.req.valid("query");
+
+        const member = await getMember({
+            databases,
+            workspaceId,
+            userId: user.$id,
+        });
+
+        if (!member) {
+            return c.json({ error: "Unauthorized" }, 401);
+        };
+
+        const query = [
+            Query.equal("workspaceId", workspaceId),
+            Query.orderDesc("$createdAt"),
+        ];
+
+        if (projectId) {
+            console.log("projectId:", projectId);
+            query.push(Query.equal("projectId", projectId));
+        };
+
+        if (status) {
+            console.log("status:", status);
+            query.push(Query.equal("status", status));
+        };
+
+        if (assigneeId) {
+            console.log("assigneeId:", assigneeId);
+            query.push(Query.equal("assigneeId", assigneeId));
+        };
+
+        if (dueDate) {
+            console.log("dueDate:", dueDate);
+            query.push(Query.equal("dueDate", dueDate));
+        };
+
+        if (search) {
+            console.log("search:", search);
+            query.push(Query.equal("name", search));
+        };
+
+        const tasks = await databases.listDocuments<Task>(
+            DATABASE_ID,
+            TASKS_ID,
+            query,
+        );
+
+        const projectIds = tasks.documents.map((task) => task.projectId);
+        const assigneeIds = tasks.documents.map((task) => task.assigneeId);
+
+        const projects = await databases.listDocuments<Project>(
+            DATABASE_ID,
+            PROJECTS_ID,
+            projectIds.length > 0 ? [Query.contains("$id", projectIds)] : []
+        );
+
+        const members = await databases.listDocuments<Project>(
+            DATABASE_ID,
+            MEMBERS_ID,
+            assigneeIds.length > 0 ? [Query.contains("$id", assigneeIds)] : []
+        );
+
+        const assignees = await Promise.all(
+            members.documents.map(async (member) => {
+                const user = await users.get(member.userId);
+
+                return {
+                    ...member,
+                    name: user.name,
+                    email: user.email,
+                }
+            })
+        );
+
+        const populatedTasks = tasks.documents.map((task) => {
+            const project = projects.documents.find(
+                (project) => project.$id === task.projectId,
+            );
+
+            const assignee = assignees.find(
+                (assignee) => assignee.$id === task.assigneeId,
+            );
+
+            return {
+                ...task,
+                project,
+                assignee,
+            };
+        });
+
+        return c.json({
+            data: {
+                ...tasks,
+                documents: populatedTasks,
+            },
+        });
+    }
+).post(
     "/",
     sessionMiddleware,
     zValidator("json", createTaskSchema),
